0d38a728fe5a1b48b15a068edc4e21ec100b735d
Raffaello Perez De Vera <raffaello.perezdevera@scout24.com>
2021-02-15 00:39:41 +0100

    Added comments to / endpoint

--- pastastore/blueprints/root.py
+++ pastastore/blueprints/root.py
@@ -3,6 +3,9 @@ from flask import Blueprint
 root_blueprint = Blueprint('root_blueprint', __name__)
 
 
-@root_blueprint.route('/')
+@root_blueprint.route('/', methods=['GET'])
 def root():
+    '''
+    Handling the / endpoint
+    '''
     return "Welcome to PastaStore!"
--- pastastore/recipes.py
+++ pastastore/recipes.py
@@ -1,3 +1,5 @@
+# RECIPES holds the allowed recipes
 RECIPES = ["lasagne", "carbonara"]
 
+# RECIPE_COUNTS holds the count of stored recipes
 RECIPE_COUNTS = dict({})
