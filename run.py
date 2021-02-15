73ffdb55bf5aebf4a272b37692ca34c4d4b49024
Raffaello Perez De Vera <59471853+calanco@users.noreply.github.com>
2021-02-15 17:53:38 +0100

    Fixed --debug parameter help field

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ def parse_arguments() -> (int, bool):
     parser.add_argument("--port", dest="port", type=int, default=5000,
                         help="The port where PastaStore is exposed to")
     parser.add_argument("--debug", action='store_true',
-                        help="Paramter to set PastaStore in debug mode")
+                        help="Run PastaStore in debug mode")
     args = parser.parse_args()
 
     return args.port, args.debug
